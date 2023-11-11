c4b8af36c18dd2b5ee1f233b7a9895377afa35b6
Mikayla <user@users-MacBook-Pro.local>
2023-11-11 17:21:43 -0500

    created async/await function for response & inquirer prompt for artist

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ const model = new OpenAI({
   });
   
   console.log({ model });
+
+  // Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
+  const promptFunc = async (input) => {
+    try {
+      const res = await model.call(input);
+      console.log(res);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  };
+
+// Initialization function that uses inquirer to prompt the user and returns a promise. It takes the user input and passes it through the call method
+  const init = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Ask about any musical artist:',
+      },
+    ]).then((inquirerResponse) => {
+      promptFunc(inquirerResponse.name)
+    });
+  };
+  
+  init();

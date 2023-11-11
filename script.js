2263b9c93c0d8108b9e2c71b0ff02109fe57721c
Mikayla <user@users-MacBook-Pro.local>
2023-11-11 17:06:46 -0500

    instantiated OpenAi object

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 const { OpenAI } = require('langchain/llms/openai');
 const inquirer = require('inquirer');
 require('dotenv').config();
+
+//instantiating OpenAI...creates wrapper for AI package & configuration
+
+const model = new OpenAI({ 
+    openAIApiKey: process.env.OPENAI_API_KEY, 
+    temperature: 0,
+    model: 'gpt-3.5-turbo'
+  });
+  
+  console.log({ model });
